984cdce56e98dca04e91adf3ef76416771a9678e
Jesse Weaver <pianohacker@gmail.com>
2013-05-22 19:39:01 -0600

    Parentheses grr

--- koha-tmpl/opac-tmpl/prog/en/js/overdrive.js
+++ koha-tmpl/opac-tmpl/prog/en/js/overdrive.js
@@ -7,46 +7,46 @@ KOHA.OverDrive = ( function() {
     var library_base_url = 'http://api.overdrive.com/v1/libraries/';
     return {
         Get: function( url, params, callback ) {
-            $.ajax({
+            $.ajax( {
                 type: 'GET',
-                url: url.replace(/https?:\/\/api.overdrive.com\/v1/, proxy_base_url),
+                url: url.replace( /https?:\/\/api.overdrive.com\/v1/, proxy_base_url ),
                 dataType: 'json',
                 data: params,
-                error: function(xhr, error) { 
+                error: function( xhr, error ) { 
                     try {
-                        callback(JSON.parse(xhr.responseText));
+                        callback( JSON.parse( xhr.responseText ));
                     } catch ( e ) {
-                        callback({error: xhr.responseText || true});
+                        callback( {error: xhr.responseText || true} );
                     }
                 },
                 success: callback
-            });
+            } );
         },
         GetCollectionURL: function( library_id, callback ) {
-            if (KOHA.OverDrive.collection_url) {
-                callback(KOHA.OverDrive.collection_url);
+            if ( KOHA.OverDrive.collection_url ) {
+                callback( KOHA.OverDrive.collection_url );
                 return;
             }
 
             KOHA.OverDrive.Get(
                 library_base_url + library_id,
                 {},
-                function (data) {
-                    if (data.error) {
-                        callback(data);
+                function ( data ) {
+                    if ( data.error ) {
+                        callback( data );
                         return;
                     }
 
                     KOHA.OverDrive.collection_url = data.links.products.href;
 
-                    callback(data.links.products.href);
+                    callback( data.links.products.href );
                 }
             );
         },
         Search: function( library_id, q, limit, offset, callback ) {
             KOHA.OverDrive.GetCollectionURL( library_id, function( data ) {
-                if (data.error) {
-                    callback(data);
+                if ( data.error ) {
+                    callback( data );
                     return;
                 }
 
