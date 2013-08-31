3c4847dc0b19b40ba6da6730e8db75020a463757
Jesse Weaver <pianohacker@gmail.com>
2013-08-30 18:01:45 -0600

    Minor bugfixes and cleanup, start of preferences system

---
+++ koha-tmpl/intranet-tmpl/lib/koha/cateditor/preferences.js
@@ -0,0 +1,27 @@
+define( function() {
+    var Preferences = {
+        Load: function( borrowernumber ) {
+            var saved_prefs;
+            try {
+                saved_prefs = JSON.parse( $.cookie( 'cateditor_preferences_' + borrowernumber ) );
+            } catch (e) {}
+
+            Preferences.user = $.extend( {
+                // Preference defaults
+                field_widgets: true,
+            }, saved_prefs );
+        },
+
+        Save: function( borrowernumber ) {
+            if ( !Preferences.user ) Preferences.Load(borrowenumber);
+
+            $.cookie( 'cateditor_preferences_' + borrowernumber, JSON.stringify(Preferences.user), { expires: 365, path: '/' } );
+
+            Preferences.user = $.extend( {
+                field_widgets: true,
+            }, saved_prefs );
+        },
+    };
+
+    return Preferences;
+} );
