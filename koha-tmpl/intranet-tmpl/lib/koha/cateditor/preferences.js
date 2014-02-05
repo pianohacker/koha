c97e7923caf85fc3cf901acb8213804cfd4b94ff
Jesse Weaver <pianohacker@gmail.com>
2014-02-04 17:33:07 -0700

    Basic autosaving macro editor

--- koha-tmpl/intranet-tmpl/lib/koha/cateditor/preferences.js
+++ koha-tmpl/intranet-tmpl/lib/koha/cateditor/preferences.js
@@ -1,6 +1,7 @@
 define( function() {
     var Preferences = {
         Load: function( borrowernumber ) {
+            if ( !borrowernumber ) return;
             var saved_prefs;
             try {
                 saved_prefs = JSON.parse( localStorage[ 'cateditor_preferences_' + borrowernumber ] );
@@ -16,6 +17,7 @@ define( function() {
         },
 
         Save: function( borrowernumber ) {
+            if ( !borrowernumber ) return;
             if ( !Preferences.user ) Preferences.Load(borrowernumber);
 
             localStorage[ 'cateditor_preferences_' + borrowernumber ] = JSON.stringify(Preferences.user);
