d73aaa4c4b9da6d263ae51219fe804e041b22340
Jesse Weaver <pianohacker@gmail.com>
2014-03-10 15:57:16 +0100

    Bug 10486 - QA followup: Remove given/when, update license and datatables
    QA script note: There are several failures related to [% interface %]
    versus opac-tmpl. According to a discussion with Owen Leonard on IRC,
    these can be ignored in this case.

--- admin/external_targets.pl
+++ admin/external_targets.pl
@@ -1,24 +1,24 @@
 #!/usr/bin/perl
 #
-# Copyright 2013 Jesse Weaver
-#
 # This file is part of Koha.
 #
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
+# Copyright 2013 Jesse Weaver
+# 
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with Koha; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
 #
 
-use Modern::Perl '2010';
+use Modern::Perl;
 
 use CGI;
 
@@ -43,12 +43,16 @@ our ( $template, $loggedinuser, $cookie ) = get_template_and_user( {
 our $op = $input->param( 'op' ) // 'show';
 $template->{VARS}->{op} = $op;
 
-given ( $op ) {
-    when ( 'show' ) { show_external_targets(); }
-    when ( 'add' ) { show_edit_form(); }
-    when ( 'edit' ) { show_edit_form(); }
-    when ( 'save' ) { save_target(); }
-    when ( 'delete' ) { delete_target(); }
+if ( $op eq 'show' ) {
+    show_external_targets();
+} elsif ( $op eq 'add' ) {
+    show_edit_form();
+} elsif ( $op eq 'edit' ) {
+    show_edit_form();
+} elsif ( $op eq 'save' ) {
+    save_target();
+} elsif ( $op eq 'delete' ) {
+    delete_target();
 }
 
 output_html_with_http_headers $input, $cookie, $template->output;
