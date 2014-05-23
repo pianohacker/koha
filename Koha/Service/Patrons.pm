fed1c4bf1cf09431a99a31ca9c7fede738f31386
Taylor Schmidt <taschmid@mymail.mines.edu>
2014-05-23 11:39:24 -0600

    added API functionality for checking out items

--- Koha/Service/Patrons.pm
+++ Koha/Service/Patrons.pm
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 package Koha::Service::Patrons;
 
 # This file is part of Koha.
@@ -40,6 +39,8 @@ use Modern::Perl;
 use base 'Koha::Service';
 
 use C4::Biblio;
+use C4::Context;
+use C4::Dates;
 use C4::Members;
 use C4::Reserves;
 
@@ -50,11 +51,54 @@ sub new {
     return $class->SUPER::new( {
         needed_flags => { circulate => 'circulate_remaining_permissions' },
         routes => [
+            [ qr'POST /(\d+)/checkouts', 'add_checkout']
             [ qr'GET /(\d+)/checkouts', 'get_checkouts' ],
             [ qr'GET /(\d+)/holds', 'get_holds' ],
             [ qr'GET /(\d+)/patronInfo', 'get_patron_info' ],
         ]
     } );
+}
+
+=head2 get_checkouts
+
+=over 4
+
+POST /svc/patrons/BORROWERNUMBER/checkouts
+
+=back
+
+Checks out an item
+
+=cut
+
+sub add_checkouts {
+    my ( $self, $borrowernumber ) = @_;
+
+    my $datedue;
+    my $duedatespec = $self->query->param('duedate');
+
+    if ( C4::Context->preference('SpecifyDueDate') && $duedatespec ){
+        if ($duedatespec =~ C4::Dates->regexp('syspref')) {
+            $datedue = dt_from_string($duedatespec);
+        } else {
+            return {errors => {INVALID_DATE=>$duedatespec}};
+        }
+    }
+
+    my ($barcode) = $self->require_params('barcode');
+
+    my $borrower = GetMember( borrowernumber => $borrowernumber );
+
+    my ( $errors, $questions, $alerts ) =
+    CanBookBeIssued( $borrower, $barcode, $datedue );
+
+    if ( %$errors || ( %$questions && !$self->query->param('confirmed') ) ) {
+        return { errors => $errors, questions => $questions, alerts => $alerts };
+    }
+
+    AddIssue( $borrower, $barcode, $datedue );
+
+    return {};
 }
 
 =head2 get_holds
